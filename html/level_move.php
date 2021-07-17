c8a0960cabbe4d467b32d200544ba58f5b025d2a
sladuf <04fud27@gmail.com>
2021-07-18 01:38:06 +0900

    21.07.17 manager login form

--- html/level_move.php
+++ html/level_move.php
@@ -2,6 +2,8 @@
 
     $temp = $_GET["level_name"];
 
-    echo "<script>alert('levle_name is {$temp}');</script>";
-    echo "<script>history.back();</script>";
+    //echo "<script>alert('levle_name is {$temp}');</script>";
+    //echo "<script>history.back();</script>";
+    echo "<script>location.replace('mp3_page.html');</script>"
+
 ?>
