1842cf088bcace380925bfbec2a97b739723df89
sladuf <04fud27@gmail.com>
2021-07-22 02:57:19 +0900

    manage student modify (21.07.21)

---
+++ html/manager/level_search.php
@@ -0,0 +1,20 @@
+<?php
+    
+    include '../session.php';
+
+    $st_level = $_GET["st_level"];
+ 
+    $mysqli = new mysqli('localhost', 'topedu', 'topedu2021!', 'english');
+
+    $search = "SELECT * FROM student WHERE level = '$st_level' and branch = '$branch'";
+    $res = $mysqli->query($search);
+    $return = array();
+
+    while($row = $res->fetch_array(MYSQLI_ASSOC)){
+        array_push($return, $row);
+    }
+
+    echo(json_encode($return));
+
+    mysqli_close($mysqli);
+?>
