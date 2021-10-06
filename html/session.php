f27c7627990e43c098fa8160e9f4f07bdabf830d
sladuf <04fud27@gmail.com>
2021-10-06 23:07:28 +0900

    All session change

--- html/session.php
+++ html/session.php
@@ -7,4 +7,10 @@ $branch = $_SESSION['branch'];
 if($_SESSION['level']){
     $level = $_SESSION['level'];
 }
+
+
+if(!($_SESSION['name'])){
+    echo "<script> alert(\"session error : 로그인이 필요합니다.\");
+               location.replace('index.html');</script>";
+}
 ?>
